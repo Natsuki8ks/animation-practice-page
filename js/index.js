1d084b893bd184b200b1fa3173f8fc9715a6e9a7
Natsuki <nkondo3@gmail.com>
2020-12-26 17:38:27 +0900

    maps

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 const tl = gsap.timeline({ default: { ease: 'power1.out' } });
 
-tl.to('.text', { y: "-20%", duration: 2, stagger: 0.2, scale: 1.05 });
+tl.to('.text', { y: "-20%", duration: 1, stagger: 0.2, scale: 1.05 });
 // ??
 
 tl.to('.intro', { y: "-100%", duration: 1 }, "-=1");
@@ -8,7 +8,7 @@ tl.to('.intro', { y: "-100%", duration: 1 }, "-=1");
 
 
 
-tl.to('.slider', { y: "-100%", duration: 1.5, delay: 0.4});
+tl.to('.slider', { y: "-100%", duration: 1, delay: 0.4});
 
 tl.to('nav', {opacity: 0.8}, {opacity: 1, duration: 1}, '-=1');
 
@@ -16,66 +16,18 @@ tl.to('nav', {opacity: 0.8}, {opacity: 1, duration: 1}, '-=1');
 
 
 
-// gsap.from('.l-title', {opacity: 0, duration: 1,  x: -100, ease: 'Power1 easeInOut'});
-
-
-// gsap.from('.context', {y: -100, opacity: 0, duration: 1},  '-=1')
-
-// gsap.from('.content-img', {x: -100, opacity: 0, duration: 1.5,start: 'bottom center'})
-
-gsap.from('.about-content', { duration: 2, start: 'top center', x: 100, toggleActions: 'restart pause resume  pause' })
-
-let tl1 = gsap.timeline({
+let tl2 = gsap.timeline({
   scrollTrigger: {
-    trigger: 'l-title',
-    start: 'top center',
-    toggleActions: 'restart pause none  pause',
+    trigger: '.content',
+    start: 'top top'
   }
 });
-tl1.from('l-title', { x: 100, opacity: 0, duration: 1.5, start: 'top center' })
-  .from('.content-img2', { x: -100, opacity: 0, duration: 1 },)
-  .from('.context2', { y: -100, opacity: 0, duration: 1 }, '-=1')
+
+tl2.from('img', {x: 300, opacity: 0, duration: 1}, '-=1')
+.from('.l-title', {x: 300, opacity: 0, duration: 1}, '+=1')
+  .from('.contents', {y: 300, opacity: 0, duration: 1}, '-=1')
 
 
-
-// gsap.from('.services-content', {duration: 3, start: 'top center', x: 100, toggleActions: 'restart none play pause'})
-
-// let tl3 = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: 'services-conetent',
-//     start: 'top center',
-//     toggleActions: 'restart pause resume  pause',
-//   }
-// });
-// tl3.from('services-content', {x: 100, opacity: 0, duration: 1.5, start: 'bottom center'})
-//   .from('.content-img3', {x: -100, opacity: 0, duration: 1}, )
-//   .from('.context3', {y: -100, opacity: 0, duration: 1}, '-=1')
-
-
-
-
-// gsap.to('services-content', {
-//   scrollTrigger: {
-//     trigger: '.services-content',
-//     start: 'top center',
-//     toggleActions: 'restart pause reverse pause',
-//     end: 'top 100px'
-//   },
-//   x: 100,
-//   opacity: 0,
-//   duration: 3
-// });
-
-
-let tl3 = gsap.timeline90;
-tl2.from('.services-content', { sPercent: -100 })
-  .from('.content-img3', { xPercent: 100 })
-  .from('.context3', { yPercent: -100 });
-
-
-
-// <div id='footer-copy'> &copy; {new Date().getFullYear()} All right reserved.</div>
-// <!-- Latansani © 新しい年を表示 -->
 
 
 
--- js/map.js
+++ js/map.js
@@ -1,8 +1,23 @@
-let map;
+function initMap(){
+  let options = {
+    zoom:8,
+    center:{lat:42.3601,lng:-71.0589} 
+  }
+  let map = new 
+  google.maps.Map(document.getElementById("map"), options);
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat : -34.397, lng: 150.644},
-    zoom: 8,
+  //marker
+  let marker = new google.maps.Map({
+    position:{lat:42.4668,lng:-70.9495},
+    map:map,
+    // icon:'anything you want'
+  });
+
+  let infoWindow = new google.maps.infoWindow({
+    content:'<h1>Here is here</h1>'
+  });
+
+  marker.addEventListenter('click', function(){
+    infoWindow.open(map, marker);
   });
 }
